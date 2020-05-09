31363e3a439acf63a6f9ef77324707ef9b4774e7
Zoechbauer <hans.zoechbauer@aon.at>
2020-05-09 16:08:41 +0200

    Working on SignUp: email & password added

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
-const MaterialModules = [MatButtonModule, MatIconModule];
+const MaterialModules = [
+  MatButtonModule,
+  MatIconModule,
+  MatFormFieldModule,
+  MatInputModule,
+];
 
 @NgModule({
   imports: [MaterialModules],
