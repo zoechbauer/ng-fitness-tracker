9e7dae3565e12e1ec6df06d61567d10206d5eb94
Zoechbauer <hans.zoechbauer@aon.at>
2020-05-11 16:05:33 +0200

    Working on Navigation & Sidenav: Adding Navigation items

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -8,6 +8,7 @@ import { MatNativeDateModule } from '@angular/material/core';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
 
 const MaterialModules = [
   MatButtonModule,
@@ -19,6 +20,7 @@ const MaterialModules = [
   MatCheckboxModule,
   MatSidenavModule,
   MatToolbarModule,
+  MatListModule,
 ];
 
 @NgModule({
