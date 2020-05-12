618599eb2ee498ef1d95599cdd7dadbfb2d013d9
Zoechbauer <hans.zoechbauer@aon.at>
2020-05-12 09:59:07 +0200

    Working on Training: Show Training screen after selecting workout

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-new-training',
   templateUrl: './new-training.component.html',
-  styleUrls: ['./new-training.component.css']
+  styleUrls: ['./new-training.component.css'],
 })
 export class NewTrainingComponent implements OnInit {
+  @Output() startTraining = new EventEmitter<void>();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onStartTraining() {
+    this.startTraining.emit();
   }
-
 }
