8875a5ded90b9853636ba13a4cb1355ae2bd8ca3
Zoechbauer <hans.zoechbauer@aon.at>
2020-05-13 15:19:43 +0200

    Working on Training: progress-spinner duration depends on duration of exercises

--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { StopTrainingComponent } from './stop-training.component';
+import { TrainingService } from '../training.service';
 
 @Component({
   selector: 'app-current-training',
@@ -12,19 +13,24 @@ export class CurrentTrainingComponent implements OnInit {
   progress = 0;
   timer: number;
 
-  constructor(private dialog: MatDialog) {}
+  constructor(
+    private dialog: MatDialog,
+    private trainingService: TrainingService
+  ) {}
 
   ngOnInit(): void {
     this.startOrResumeTimer();
   }
 
   startOrResumeTimer() {
+    const step =
+      (this.trainingService.getRunningExercise().duration / 100) * 1000;
     this.timer = setInterval(() => {
-      this.progress += 5;
+      this.progress += 1;
       if (this.progress >= 100) {
         clearInterval(this.timer);
       }
-    }, 1000);
+    }, step);
   }
 
   onStop() {
--- src/app/training/training.service.ts
+++ src/app/training/training.service.ts
@@ -24,4 +24,8 @@ export class TrainingService {
     );
     this.exerciseChanged.next({ ...this.runningExercise });
   }
+
+  getRunningExercise() {
+    return { ...this.runningExercise };
+  }
 }
